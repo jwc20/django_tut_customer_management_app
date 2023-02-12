e10143bab5e8c67c18b9ba3465478b832640e5f4
jwc20 <choij2015@gmail.com>
2023-02-12 12:38:58 +0900

    created home and about page

--- crm1/crm1/urls.py
+++ crm1/crm1/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home Page')
+
+def contact(request):
+    return HttpResponse('Contact Page')
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home),
+    path("contact/", contact),
 ]
