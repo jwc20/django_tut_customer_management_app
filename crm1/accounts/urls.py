6807c86eb9ffdee6c42a58d18fc82ea3c3cb5448
jwc20 <choij2015@gmail.com>
2023-02-12 12:53:20 +0900

    created path to the views

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", views.home),
+    path("product/", views.product),
+    path("customer/", views.customer),
 ]
