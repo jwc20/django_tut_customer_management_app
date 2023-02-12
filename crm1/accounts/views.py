ef6a5ebe5f7c4cb8bda9a34a4e57486b6fa1ceb3
jwc20 <choij2015@gmail.com>
2023-02-12 12:48:00 +0900

    made views for accounts app

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("Home Page")
+
+
+def product(request):
+    return HttpResponse("Product Page")
+
+def customer(request):
+    return HttpResponse("Customer Page")
