36d61326cd40b437df38b0f6533d069cfec0c954
jwc20 <choij2015@gmail.com>
2023-02-14 13:36:54 +0900

    set product and customer to have one to many relationship with order

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -24,6 +24,8 @@ class Product(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 
+
+
 class Order(models.Model):
     STATUS = (
         ("Pending", "Pending"),
@@ -31,7 +33,7 @@ class Order(models.Model):
         ("Delivered", "Delivered"),
     )
 
-    # customer =
-    # product =
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
